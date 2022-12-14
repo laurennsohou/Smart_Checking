74566eb2df0c2488bbf28eaaa5b8d592f89e2b74
Laurenn SOHOU <sohoulaurenn@gmail.com>
2022-12-14 16:08:11 +0000

    Smart

---
+++ app/src/main/java/com/laurenn/smart_cheking/Accueil.kt
@@ -0,0 +1,11 @@
+package com.laurenn.smart_cheking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Accueil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.accueil)
+    }
+}
---
+++ app/src/main/java/com/laurenn/smart_cheking/Setting.kt
@@ -0,0 +1,11 @@
+package com.laurenn.smart_cheking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Setting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
